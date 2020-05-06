e00773f2151446c48169b69ce009359f2b6650b6
Ánderson Ballesteros <ander.ballesteros98@gmail.com>
2020-05-06 12:44:51 -0500

    se creo la conexcion con el socket en front

--- front/src/indexScript.js
+++ front/src/indexScript.js
@@ -1,4 +1,5 @@
-
+var apiURL = "ws://25.133.184.153:8500";
+var webSocket = new WebSocket(apiURL);
 
 function joinGame(){
 	codeRoom = $("#roomCode").val();
@@ -6,6 +7,24 @@ function joinGame(){
 
 }
 
+webSocket.onopen = function(evt) {
+	setInterval(keepAlive(), 10000);
+}
+
+function keepAlive() {
+	request = {
+		type: 'keepAlive',
+		data: {}
+	}
+	webSocket.send(JSON.stringify(request));
+}
+
+webSocket.onmessage = function(JSONResponse) {
+
+}
+
 function createGame(){
 	console.log("creatingGame");
 }
+
+
